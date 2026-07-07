aa85f33b0dc50d16a971de805a660fd8bcc4a9d3
kawshashank <kawshashank@gmail.com>
2026-07-07 23:37:44 +0530

    new update for next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* any other config options here */
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "https://kashmiri-shraad.vercel.app"
       : undefined,
 };
+
+export default nextConfig;
